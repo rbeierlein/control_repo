e0bf256d8d22f4eac64a3d2593b768eb3cee69e3
rbeierlein <34692649+rbeierlein@users.noreply.github.com>
2020-02-21 17:37:03 -0500

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -16,5 +16,9 @@ class minecraft {
     ensure => file'
     source => 'puppet:///modules/minecraft/minecraft.service',
   }
+  service {'minecraft':
+    ensure => running,
+    enable => true,
+  }
   
     }
